9ab918bf121543da874716629fdf32e956db1f99
LD0265 <lmd111905@gmail.com>
2026-07-07 18:37:20 -0400

    wal changes

--- mako/update-colors.sh
+++ mako/update-colors.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -eu
-
 CACHE_FILE="$HOME/.cache/wal/mako"
+BASE_CONFIG="$HOME/.config/mako/config-base"
 MAKO_CONFIG="$HOME/.config/mako/config"
 
 [ -f "$CACHE_FILE" ] || exit 1
+[ -f "$BASE_CONFIG" ] || exit 1
 
-# Remove old generated block if present, then append fresh colors
-sed -i '/^# BEGIN PYWAL/,/^# END PYWAL/d' "$MAKO_CONFIG"
-{
-    echo "# BEGIN PYWAL"
-    cat "$CACHE_FILE"
-    echo "# END PYWAL"
-} >> "$MAKO_CONFIG"
+cat "$BASE_CONFIG" "$CACHE_FILE" > "$MAKO_CONFIG"
 
 makoctl reload
